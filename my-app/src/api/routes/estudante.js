f69f35c9d93c635e44619c3017acc180a07d835a
Asafe Domingues Marcelino <66213067+Asafesseidon@users.noreply.github.com>
2025-10-03 10:30:43 -0300

    Update estudante.js

--- my-app/src/api/routes/estudante.js
+++ my-app/src/api/routes/estudante.js
@@ -5,17 +5,16 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { verifyToken, isAdmin } = require('../middlewares/auth');
 
-/* GET - Buscar todos os usuários */
-// requer usuário autenticado como admin
-router.get('/', verifyToken, isAdmin, async function(req, res) {
+/* GET - Buscar todos os estudantes */
+router.get('/', verifyToken, async function(req, res) {
   try {
-    const result = await pool.query('SELECT id, login, email, role FROM usuario ORDER BY id');
+    const result = await pool.query('SELECT * FROM estudante ORDER BY id');
     res.json({
       success: true,
       data: result.rows
     });
   } catch (error) {
-    console.error('Erro ao buscar usuários:', error);
+    console.error('Erro ao buscar os estudantes:', error);
     // http status 500 - Internal Server Error
     res.status(500).json({
       success: false,
@@ -24,40 +23,12 @@ router.get('/', verifyToken, isAdmin, async function(req, res) {
   }
 });
 
-/* GET parametrizado - Buscar usuário autenticado */
+/* GET parametrizado - Buscar funcionário autenticado */
 router.get('/me', verifyToken, async function(req, res) {
   try {
     // parâmetro obtido do token pelo middleware
     const id = req.user.id;
-    const result = await pool.query('SELECT id, login, email FROM estudante WHERE id = $1', [id]);
-
-    if (result.rows.length === 0) {
-      // http status 404 - Not Found
-      return res.status(404).json({
-        success: false,
-        message: 'Usuário não encontrado'
-      });
-    }
-    
-    res.json({
-      success: true,
-      data: result.rows[0]
-    });
-  } catch (error) {
-    console.error('Erro ao buscar estudante:', error);
-    // http status 500 - Internal Server Error
-    res.status(500).json({
-      success: false,
-      message: 'Erro interno do servidor'
-    });
-  }
-});
-
-/* GET parametrizado - Buscar usuário por ID */
-router.get('/:id', verifyToken, isAdmin, async function(req, res) {
-  try {
-    const { id } = req.params;
-    const result = await pool.query('SELECT id, login, email FROM usuario WHERE id = $1', [id]);
+    const result = await pool.query('SELECT * FROM Funcionario WHERE id = $1', [id]);
 
     if (result.rows.length === 0) {
       // http status 404 - Not Found
@@ -81,31 +52,59 @@ router.get('/:id', verifyToken, isAdmin, async function(req, res) {
   }
 });
 
-/* POST - Criar novo usuário */
-router.post('/', verifyToken, isAdmin, async function(req, res) {
+/* GET parametrizado - Buscar usuário por ID */
+router.get('/:id', verifyToken, async function(req, res) {
   try {
-    const { login, email, senha, role = 'user' } = req.body;
-    
-    // Validação básica
-    if (!login || !email || !senha ) {
-      // http status 400 - Bad Request
-      return res.status(400).json({
+    const { id } = req.params;
+    const result = await pool.query('SELECT * FROM usuario WHERE id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      // http status 404 - Not Found
+      return res.status(404).json({
         success: false,
-        message: 'Login, email e senha são obrigatórios'
+        message: 'Usuário não encontrado'
       });
     }
     
-    // Verificar se o login já existe
-    const existingUser = await pool.query('SELECT id FROM usuario WHERE login = $1', [login]);
+    res.json({
+      success: true,
+      data: result.rows[0]
+    });
+  } catch (error) {
+    console.error('Erro ao buscar usuário:', error);
+    // http status 500 - Internal Server Error
+    res.status(500).json({
+      success: false,
+      message: 'Erro interno do servidor'
+    });
+  }
+});
+
+/* POST - Criar novo estudante */
+router.post('/', verifyToken, isAdmin, async function(req, res) {
+  try {
+    const { nome, nomeSocial, matricula, suspenso, serie, foto, email, turma } = req.body;
+    
+    // Validação básica
+    if (!nome  || !matricula  || !serie || !foto  || !email || !turma) {
+      // http status 400 - Bad Request
+      return res.status(400).json({
+        success: false,
+        message: 'Nome, matricula, seríe, foto e email são obrigatórios'
+      });
+    }
+    
+    // Verificar se a matricula já existe
+    const existingUser = await pool.query('SELECT id FROM estudante WHERE matricula = $1', [matricula]);
     if (existingUser.rows.length > 0) {
       return res.status(409).json({
         success: false,
-        message: 'Login já está em uso'
+        message: 'Matrícula já está em uso'
       });
     }
 
     // Verificar se o email já existe
-    const existingEmail = await pool.query('SELECT id FROM usuario WHERE email = $1', [email]);
+    const existingEmail = await pool.query('SELECT id FROM estudante WHERE email = $1', [email]);
     if (existingEmail.rows.length > 0) {
       return res.status(409).json({
         success: false,
@@ -114,21 +113,19 @@ router.post('/', verifyToken, isAdmin, async function(req, res) {
     }
 
     // Hash da senha
-    const hashedPassword = await bcrypt.hash(senha, 12);
-
     const result = await pool.query(
-      'INSERT INTO usuario (login, email, senha, role) VALUES ($1, $2, $3, $4) RETURNING id, login, email, role',
-      [login, email, hashedPassword, role]
+      'INSERT INTO usuario (nome, nomeSocial, matricula, suspenso, serie, foto, email, turma) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id, nome, nomeSocial, matricula, suspenso, serie, foto, email, turma',
+      [nome, nomeSocial, matricula, suspenso, serie, foto, email, turma]
     );
 
     // http status 201 - Created
     res.status(201).json({
       success: true,
-      message: 'Usuário criado com sucesso',
+      message: 'Estudante criado com sucesso',
       data: result.rows[0]
     });
   } catch (error) {
-    console.error('Erro ao criar usuário:', error);
+    console.error('Erro ao criar estudante:', error);
     // Verificar se é erro de constraint
     if (error.code === '23514') {
       return res.status(400).json({
@@ -143,91 +140,6 @@ router.post('/', verifyToken, isAdmin, async function(req, res) {
     });
   }
 });
-
-
-/* POST - Autenticar usuário */
-router.post('/login', async function(req, res) {
-  try {
-    const { login, password } = req.body;
-    // obtém o usuário do banco de dados
-    const result = await pool.query(`SELECT 
-      id, login, email, senha as passwordHash, role
-      FROM usuario 
-      WHERE login = $1`, [login]);
-
-    /*
-     tratar login inválido igual senha incorreta
-     confere maior segurança por não expor indiretamente
-     se existe uma conta com aquele login 
-    */
-    if (result.rows.length === 0) {
-      // https status 401 - unauthorized
-      return res.status(401).json({
-        success: false,
-        message: 'Credenciais inválidas'
-      });
-    }
-
-    // Objeto de usuário
-    const user = result.rows[0];
-
-    /*
-     verifica a senha passando senha do forntend e hash armazenada
-     a partir da hash não se pode descobrir a senha
-     mas fornecendo a senha dá para aplicar a hash e ver coincidem
-    */
-    
-    bcrypt.compare(password, user.passwordhash, (err, isMatch) => {
-      if (err) {
-        console.error('Erro no bcrypt:', err);
-        // https status 500 - internal server error
-        return res.status(500).json({
-          success: false,
-          message: 'Erro interno do servidor'
-        });
-      }
-      
-      if (!isMatch) {
-        // https status 401 - unauthorized
-        return res.status(401).json({
-          success: false,
-          message: 'Credenciais inválidas'
-        });
-      }
-
-      // Cria o token com as informações do usuário logado e sua chave pública
-      const token = jwt.sign(
-        { 
-          id: user.id, 
-          login: user.login,
-          email: user.email,
-          // tipo do usuário, que vem do banco
-          role: user.role 
-          // a senha não entra no token para não ser exposta
-        }, 
-        process.env.JWT_SECRET, //chave secreta, nunca exponha!! >>> PERIGO <<<
-        { expiresIn: '1h' } 
-      );
-
-      // O token contém as informções do usuário com a chave para posterior validação
-      return res.status(200).json({
-        success: true,
-        token: token,
-        message: 'Autenticado com sucesso!'
-      });
-    });
-
-  } catch (error) {
-    console.error('Erro ao autenticar usuário:', error);
-    // http status 500 - Internal Server Error
-    res.status(500).json({
-      success: false,
-      message: 'Erro interno do servidor'
-    });
-  }
-  
-});
-
 
 /* PUT - Atualizar usuário */
 router.put('/:id', verifyToken, isAdmin, async function(req, res) {
