4cc5cf0b031c10d158991863b645391a48bcc349
IMadeACupcakeCode <leo0607gamer@gmail.com>
2025-10-10 11:56:14 -0300

    Criação do CRUD da lista de entrada

--- src/api/routes/entrada.js
+++ src/api/routes/entrada.js
@@ -6,37 +6,54 @@ const jwt = require('jsonwebtoken');
 const { verifyToken, isAdmin } = require('../middlewares/auth');
 
 
-/* POST - Criar novo email */
+/* GET - Buscar todas as listas de entrada */
+router.get('/', verifyToken, async function(req, res) {
+    try {
+      const result = await pool.query('SELECT * FROM Entrada ORDER BY id');
+      res.json({
+        success: true,
+        data: result.rows
+      });
+    } catch (error) {
+      console.error('Erro ao buscar os ano letivos:', error);
+      // http status 500 - Internal Server Error
+      res.status(500).json({
+        success: false,
+        message: 'Erro interno do servidor'
+      });
+    }
+  });
+/* POST - Criar nova lista de entrada */
 router.post('/', verifyToken, isAdmin, async function(req, res) {
   try {
-    const { endereco, estudante} = req.body;
+    const { data, almoco, estudante} = req.body;
     
     // Validação básica
-    if (!endereco || !estudante) {
+    if (!data || !estudante) {
       // http status 400 - Bad Request
       return res.status(400).json({
         success: false,
-        message: 'Endereço e estudante são obrigatórios'
+        message: 'Data e estudante são obrigatórios'
       });
     }
     
-    // Verificar se o email existe 
-    const existingUser = await pool.query('SELECT id FROM email WHERE endereco = $1 ' , [endereco]);
+    // Verificar se essa lista de entrada existe 
+    const existingUser = await pool.query('SELECT id FROM Entrada WHERE data= $1' , [data]);
     if (existingUser.rows.length > 0) {
       return res.status(409).json({
         success: false,
-        message: 'Email já está em uso'
+        message: 'Essa lista de entrada já está em uso'
       });
     }
 
     // http status 201 - Created
     res.status(201).json({
       success: true,
-      message: 'Email registrado com sucesso',
+      message: 'Lista de entrada registrada com sucesso',
       data: result.rows[0]
     });
   } catch (error) {
-    console.error('Erro ao registrar email:', error);
+    console.error('Erro ao registrar lista de entrada:', error);
     // Verificar se é erro de constraint
     if (error.code === '23514') {
       return res.status(400).json({
@@ -52,44 +69,44 @@ router.post('/', verifyToken, isAdmin, async function(req, res) {
   }
 });
 
-/* PUT - Atualizar email */
+/* PUT - Atualizar lista de entrada */
 router.put('/:id', verifyToken, isAdmin, async function(req, res) {
   try {
     const { id } = req.params;
-    const { endereco, estudante } = req.body;
+    const { data, estudante } = req.body;
     
     // Validação básica
-    if (!endereco || !estudante) {
+    if (!data || !estudante) {
       // http status 400 - Bad Request
       return res.status(400).json({
         success: false,
-        message: 'Endereço e estudante são obrigatórios'
+        message: 'Data e estudante são obrigatórios'
       });
     }
     
     // Verificar se o email existe
-    const userExists = await pool.query('SELECT id FROM email WHERE id = $1', [id]);
+    const userExists = await pool.query('SELECT id FROM Entrada WHERE id = $1', [id]);
     if (userExists.rows.length === 0) {
       // http status 404 - Not Found
       return res.status(404).json({
         success: false,
-        message: 'Email não encontrado'
+        message: 'Lista de entrada não encontrada'
       });
     }
     
     
     let query, params;    
-    query = 'UPDATE email SET endereco = $1, estudante = $2  WHERE id = $3 RETURNING id,  endereco, estudante';
-    params = [ endereco, estudante, id];    
+    query = 'UPDATE Entrada SET data = $1, almoco = $2, estudante= $3  WHERE id = $4 RETURNING id,  data, almoco, estudante';
+    params = [ data, almoco, estudante, id];    
     const result = await pool.query(query, params);
     
     res.json({
       success: true,
-      message: 'Email atualizado com sucesso',
+      message: 'Lista de entrada atualizado com sucesso',
       data: result.rows[0]
     });
   } catch (error) {
-    console.error('Erro ao atualizar email:', error);
+    console.error('Erro ao atualizar lista de entrada:', error);
     // Verificar se é erro de constraint
     if (error.code === '23514') {
       return res.status(400).json({
@@ -105,29 +122,29 @@ router.put('/:id', verifyToken, isAdmin, async function(req, res) {
   }
 });
 
-/* DELETE - Remover email*/
+/* DELETE - Remover lista de entrada*/
 router.delete('/:id', verifyToken, isAdmin, async function(req, res) {
   try {
     const { id } = req.params;
     
     // Verificar se o usuário existe
-    const userExists = await pool.query('SELECT id FROM email WHERE id = $1', [id]);
+    const userExists = await pool.query('SELECT id FROM Entrada WHERE id = $1', [id]);
     if (userExists.rows.length === 0) {
       // http status 404 - Not Found
       return res.status(404).json({
         success: false,
-        message: 'Email não encontrado'
+        message: 'Lista de entrada não encontrada'
       });
     }
     
-    await pool.query('DELETE FROM email WHERE id = $1', [id]);
+    await pool.query('DELETE FROM Entrada WHERE id = $1', [id]);
     
     res.json({
       success: true,
-      message: 'Email deletado com sucesso'
+      message: 'Lista de entrada deletada com sucesso'
     });
   } catch (error) {
-    console.error('Erro ao deletar email:', error);
+    console.error('Erro ao deletar lista de entrada:', error);
     // http status 500 - Internal Server Error
     res.status(500).json({
       success: false,
