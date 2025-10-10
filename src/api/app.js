ac5ab5292f07c7a7a43eb4595f442e138b5bba62
JoaoGabrielPereiraLopes <gabriel.pitito123@gmail.com>
2025-10-10 10:43:31 -0300

    Ajuste fino

--- src/api/app.js
+++ src/api/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/funcionario');
 
 var app = express();
 
