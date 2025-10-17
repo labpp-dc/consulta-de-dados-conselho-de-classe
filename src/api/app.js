7b53219e26b391f59ff535c55e686cadc0208680
IMadeACupcakeCode <leo0607gamer@gmail.com>
2025-10-17 11:11:08 -0300

    Backend linkado

--- src/api/app.js
+++ src/api/app.js
@@ -36,6 +36,15 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/anoLetivo', anoLetivoRouter);
+app.use('/curso', cursoRouter);
+app.use('/email', emailRouter);
+app.use('/entrada', entradaRouter);
+app.use('/estudante', estudanteRouter);
+app.use('/ocorrencia', ocorrenciaRouter);
+app.use('/saida', saidaRouter);
+app.use('/turma', turmaRouter);
+app.use('/visitante', visitanteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
